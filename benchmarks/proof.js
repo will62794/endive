89d489e41940eb8fac27135f2b9a40fd2b6e79d4
William Schultz <will627@comcast.net>
2023-07-30 16:52:44 +0100

    interactive edits

--- benchmarks/proof.js
+++ benchmarks/proof.js
@@ -6,4 +6,14 @@ window.onload = function(){
         // $(this).children('ul').eq(1).toggle();
         // console.log($(this).children('ul').eq(0));
     })
+
+
+    $('.proof-node-expr').on('click', function(ev){
+        // TODO.
+        // $(this).children('ul').eq(1).toggle();
+        let proof_node_expr = $(this).html();
+        console.log(proof_node_expr);
+        $('.cti-box').hide();
+        $('.cti_' + proof_node_expr).show();
+    })
 }
