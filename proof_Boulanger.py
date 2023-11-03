0d72c2be2d2dc0e763821fa8cf0d1959cd83ee46
William Schultz <will627@comcast.net>
2023-11-02 22:55:32 -0400

    Add some Boulanger support lemmas

--- proof_Boulanger.py
+++ proof_Boulanger.py
@@ -10,11 +10,22 @@ def make_node(expr):
 lemmaTRUE = StructuredProofNode("LemmaTrue", "TRUE")
 lemmaTRUEShim = StructuredProofNode("LemmaTrueShim", "1=1")
 
+
+H1 = make_node("H_L1")
+H2 = make_node("H_L2")
+H3 = make_node("H_L3")
+H4 = make_node("H_L4")
+H5 = make_node("H_L5")
 H6 = make_node("H_L6")
 H7 = make_node("H_L7")
 H8 = make_node("H_L8")
 H9 = make_node("H_L9")
 
+H6.children = {
+    "w2Action": [
+        H2,H8
+    ]
+}
 # Comment out stuff below for demo.
 children = {
     "w1Action": [
