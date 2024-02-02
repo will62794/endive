90c5c25b10abc2efc94cec5c9ca04a69d6443150
William Schultz <will627@comcast.net>
2024-02-01 22:47:11 -0500

    Save depth

--- benchmarks/check_EPaxos.sh
+++ benchmarks/check_EPaxos.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 javaexe=fastjava
-$javaexe -cp tla2tools-1a92ec7.jar tlc2.TLC -noGenerateSpecTE -deadlock -simulate -depth 40 -workers 6 EPaxos | tee logout_EPaxos
+$javaexe -cp tla2tools-1a92ec7.jar tlc2.TLC -noGenerateSpecTE -deadlock -simulate -depth 30 -workers 6 EPaxos | tee logout_EPaxos
