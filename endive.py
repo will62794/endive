844fb26bc23cb18c1019bf4e51344eb015292503
William Schultz <will627@comcast.net>
2024-03-14 11:40:31 -0400

    more compact lemma nodes

--- endive.py
+++ endive.py
@@ -3908,7 +3908,11 @@ class InductiveInvGen():
                             label += ">"
                             fontsize="14pt"
                     if "is_lemma" in node:
-                        label = "< " + n
+                        # Split something like 'Inv249_R3_2_I2_0'.
+                        name_parts = n.partition("_")
+                        label = "< " + name_parts[0]
+                        if len(name_parts[2]) > 0:
+                            label += " <BR/> <FONT POINT-SIZE='12'>" + str(name_parts[2]) + " </FONT>"
                         if "order" in node:
                             # label += "<BR/>"
                             label += " <BR/> <FONT POINT-SIZE='12'>(" + str(node["order"]) + ") </FONT>"
