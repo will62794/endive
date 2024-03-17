907a9e2e317ba261d9105f02790bb09a2c1dbcce
William Schultz <will627@comcast.net>
2024-03-17 15:37:54 -0400

    Set to 48 workers

--- discovery_Hermes_script.sh
+++ discovery_Hermes_script.sh
@@ -11,7 +11,7 @@ module load OpenJDK/19.0.1
 cd endive
 git pull --rebase
 srun python3 endive.py --spec benchmarks/Hermes \
-    --seed 2444 --num_simulate_traces 30000 --tlc_workers 52 \
+    --seed 2444 --num_simulate_traces 30000 --tlc_workers 48 \
     --debug --target_sample_time_limit_ms 10000 --target_sample_states 10000 \
     --opt_quant_minimize --k_cti_induction_depth 1 --ninvs 40000 --max_num_ctis_per_round 1000 \
     --auto_lemma_action_decomposition --save_dot --max_num_conjuncts_per_round 16 --niters 4 --override_num_cti_workers 12 \
