53d87541ffbbaa5648e966fa71f3cb4c8f41b213
William Schultz <will627@comcast.net>
2024-03-17 14:13:56 -0400

    update script

--- discovery_run_Hermes.sh
+++ discovery_run_Hermes.sh
@@ -11,8 +11,8 @@ module load OpenJDK/19.0.1
 cd endive
 git pull --rebase
 srun python3 endive.py --spec benchmarks/Hermes \
-    --seed 2444 --num_simulate_traces 30000 --tlc_workers 7 \
+    --seed 2444 --num_simulate_traces 30000 --tlc_workers 42 \
     --debug --target_sample_time_limit_ms 10000 --target_sample_states 10000 \
     --opt_quant_minimize --k_cti_induction_depth 1 --ninvs 40000 --max_num_ctis_per_round 1000 \
-    --auto_lemma_action_decomposition --save_dot --max_num_conjuncts_per_round 16 --niters 4 --override_num_cti_workers 5 \
+    --auto_lemma_action_decomposition --save_dot --max_num_conjuncts_per_round 16 --niters 4 --override_num_cti_workers 12 \
     --enable_partitioned_state_caching --proof_tree_mode --nrounds 45 --persistent --tlc_jar tla2tools.jar | tee logout
