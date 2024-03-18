e81a7a0e87066694494baaf0cb410bab37deb860
William Schultz <will627@comcast.net>
2024-03-17 23:26:20 -0400

    Save Hermes script

--- discovery_Hermes_script.sh
+++ discovery_Hermes_script.sh
@@ -14,7 +14,10 @@ srun python3 endive.py --spec benchmarks/Hermes \
     --seed 2444 --num_simulate_traces 30000 --tlc_workers 32 \
     --debug --target_sample_time_limit_ms 10000 --target_sample_states 10000 \
     --opt_quant_minimize --k_cti_induction_depth 1 \
-    --ninvs 40000 --max_num_ctis_per_round 1000 \
+    --ninvs 80000 --max_num_ctis_per_round 1000 \
     --auto_lemma_action_decomposition --save_dot --max_num_conjuncts_per_round 16 --niters 4 --override_num_cti_workers 5 \
-    --enable_partitioned_state_caching --proof_tree_mode --nrounds 45 \
-    --action_filter "HRcvValAction,HSendValsAction,HWriteAction,HRcvInvAction,HCoordWriteReplayAction,NodeFailureAction,HFollowerWriteReplayAction,HReadAction"
+    --enable_partitioned_state_caching --include_existing_conjuncts --proof_tree_mode \
+    --nrounds 45 \
+    --action_filter "HRcvValAction,HSendValsAction,HWriteAction,HRcvInvAction,HCoordWriteReplayAction,NodeFailureAction"
+
+# --action_filter "HRcvValAction,HSendValsAction,HWriteAction,HRcvInvAction,HCoordWriteReplayAction,NodeFailureAction,HFollowerWriteReplayAction,HReadAction"
