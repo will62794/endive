926e7d99d70c463eff2b562076102f693a37dc47
William Schultz <will627@comcast.net>
2024-03-21 17:22:18 -0400

    Save worker counter

--- discovery_Boulanger_script.sh
+++ discovery_Boulanger_script.sh
@@ -22,7 +22,7 @@ git pull --rebase
 srun python3 endive.py --spec benchmarks/$specname \
     --seed 2444 --num_simulate_traces 100000 --tlc_workers 32 \
     --debug --target_sample_time_limit_ms 10000 --target_sample_states 20000 \
-    --opt_quant_minimize --k_cti_induction_depth 1 --override_num_cti_workers 12 \
+    --opt_quant_minimize --k_cti_induction_depth 1 --override_num_cti_workers 4 \
     --ninvs 40000 --max_num_ctis_per_round 1000 \
     --save_dot --max_num_conjuncts_per_round 8 --niters 4 \
     --auto_lemma_action_decomposition --enable_partitioned_state_caching --proof_tree_mode \
