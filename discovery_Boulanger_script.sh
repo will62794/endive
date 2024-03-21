4a83720cab5c01313357c63e07c252338b734084
William Schultz <will627@comcast.net>
2024-03-21 11:19:54 -0400

    Update Boulanger script

--- discovery_Boulanger_script.sh
+++ discovery_Boulanger_script.sh
@@ -19,17 +19,10 @@ git clone -b ind-tree https://github.com/will62794/endive.git
 cd endive
 git pull --rebase
 srun python3 endive.py --spec benchmarks/$specname \
-    --seed 2444 --num_simulate_traces 120000 --tlc_workers 32 \
-    --debug --target_sample_time_limit_ms 10000 --target_sample_states 10000 \
+    --seed 2444 --num_simulate_traces 100000 --tlc_workers 32 \
+    --debug --target_sample_time_limit_ms 10000 --target_sample_states 20000 \
     --opt_quant_minimize --k_cti_induction_depth 1 --override_num_cti_workers 12 \
     --ninvs 40000 --max_num_ctis_per_round 1000 \
-    --auto_lemma_action_decomposition --save_dot --max_num_conjuncts_per_round 8 --niters 4 \
-    --enable_partitioned_state_caching --proof_tree_mode \
+    --save_dot --max_num_conjuncts_per_round 8 --niters 4 \
+    --auto_lemma_action_decomposition --enable_partitioned_state_caching --proof_tree_mode \
     --nrounds 45
-
-
-# --action_filter "HRcvValAction,HSendValsAction,HWriteAction,HRcvInvAction,HCoordWriteReplayAction,NodeFailureAction,HFollowerWriteReplayAction,HReadAction,HRcvAckAction"
-# --action_filter "HRcvValAction,HSendValsAction,HWriteAction,HRcvInvAction,HCoordWriteReplayAction,NodeFailureAction,HFollowerWriteReplayAction,HReadAction"
-
-
-# HRcvValAction,HSendValsAction,HWriteAction,HRcvInvAction,HRcvAckAction,HRcvInvNewerAction,HCoordWriteReplayAction,NodeFailureAction,HFollowerWriteReplayAction,HReadAction
