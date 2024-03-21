4d70ddf9896e5b4913f5c87ed2fee78fd31158d2
William Schultz <will627@comcast.net>
2024-03-21 10:41:05 -0400

    Update fetcher

--- discovery_fetch_data.sh
+++ discovery_fetch_data.sh
@@ -7,7 +7,6 @@ mkdir -p discovery_data
 scp -O -r neudiscovery:/home/schultz.w/endive_logs discovery_data
 
 # Also fetch Hermes,AsyncRaft proof stuff.
-scp -O -r neudiscovery:/home/schultz.w/endive/benchmarks/Hermes_ind-proof-tree discovery_data
 scp -O -r neudiscovery:/home/schultz.w/endive/benchmarks/Hermes_ind-proof-tree.pdf discovery_data
-scp -O -r neudiscovery:/home/schultz.w/benchmarking/AsyncRaft/endive/benchmarks/AsyncRaft_ind-proof-tree discovery_data
 scp -O -r neudiscovery:/home/schultz.w/benchmarking/AsyncRaft/endive/benchmarks/AsyncRaft_ind-proof-tree.pdf discovery_data
+scp -O -r neudiscovery:/home/schultz.w/benchmarking/Boulanger/endive/benchmarks/Boulanger_ind-proof-tree.pdf discovery_data
