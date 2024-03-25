923e5c9579a41d68f038a838a459942c38b7bfec
William Schultz <will627@comcast.net>
2024-03-25 14:23:46 -0400

    Save scripts

--- discovery_scripts/discovery_Boulanger_script.sh
+++ discovery_scripts/discovery_Boulanger_script.sh
@@ -2,7 +2,7 @@
 #SBATCH -J Boulanger_endive_Job               # Job name
 #SBATCH --partition short    # Partition name
 #SBATCH --time=12:00:00       # Time limit.
-#SBATCH --constraint=cascadelake       # Newer processors.
+#SBATCH --constraint="[broadwell|cascadelake]"       # Newer processors.
 #SBATCH -N 1                   # Number of nodes
 #SBATCH -n 1                   # Number of tasks
 #SBATCH --cpus-per-task 36                  # Number of cores
@@ -21,9 +21,9 @@ cd endive
 git pull --rebase
 srun python3 endive.py --spec benchmarks/$specname \
     --seed 2444 --num_simulate_traces 100000 --tlc_workers 32 \
-    --debug --target_sample_time_limit_ms 10000 --target_sample_states 20000 \
-    --opt_quant_minimize --k_cti_induction_depth 1 --override_num_cti_workers 4 \
-    --ninvs 40000 --max_num_ctis_per_round 1000 \
+    --debug --target_sample_time_limit_ms 10000 --target_sample_states 30000 \
+    --opt_quant_minimize --k_cti_induction_depth 1 --override_num_cti_workers 8 \
+    --ninvs 40000 --max_num_ctis_per_round 2000 \
     --save_dot --max_num_conjuncts_per_round 8 --niters 4 \
     --auto_lemma_action_decomposition --enable_partitioned_state_caching --proof_tree_mode \
     --nrounds 45
--- discovery_scripts/discovery_Hermes_script.sh
+++ discovery_scripts/discovery_Hermes_script.sh
@@ -2,9 +2,10 @@
 #SBATCH -J Hermes_endive_Job               # Job name
 #SBATCH --partition short    # Partition name
 #SBATCH --time=12:00:00       # Time limit.
+#SBATCH --constraint="[broadwell|cascadelake]"       # Newer processors.
 #SBATCH -N 1                   # Number of nodes
 #SBATCH -n 1                   # Number of tasks
-#SBATCH --cpus-per-task 36                  # Number of cores
+#SBATCH --cpus-per-task 32                  # Number of cores
 #SBATCH -o endive_logs/Hermes/Hermes_output_%j.txt       # Standard output file
 #SBATCH -e endive_logs/Hermes/Hermes_error_%j.txt        # Standard error file
 
@@ -12,14 +13,16 @@ module load OpenJDK/19.0.1
 cd endive
 git pull --rebase
 srun python3 endive.py --spec benchmarks/Hermes \
-    --seed 2444 --num_simulate_traces 120000 --tlc_workers 32 \
+    --seed 2444 --num_simulate_traces 120000 --tlc_workers 24 \
     --debug --target_sample_time_limit_ms 10000 --target_sample_states 10000 \
-    --opt_quant_minimize --k_cti_induction_depth 1 --override_num_cti_workers 12 \
+    --opt_quant_minimize --k_cti_induction_depth 1 --override_num_cti_workers 4 \
     --ninvs 40000 --max_num_ctis_per_round 1000 \
-    --auto_lemma_action_decomposition --save_dot --max_num_conjuncts_per_round 8 --niters 4 \
-    --enable_partitioned_state_caching --proof_tree_mode \
-    --nrounds 45 \
-    --action_filter "HRcvValAction,HSendValsAction,HWriteAction,HRcvInvAction,HRcvAckAction,HRcvInvNewerAction"
+    --save_dot --max_num_conjuncts_per_round 8 --niters 4 \
+    --auto_lemma_action_decomposition --enable_partitioned_state_caching --proof_tree_mode \
+    --nrounds 45
+
+
+# --action_filter "HRcvValAction,HSendValsAction,HWriteAction,HRcvInvAction,HRcvAckAction,HRcvInvNewerAction"
 
 
 # --action_filter "HRcvValAction,HSendValsAction,HWriteAction,HRcvInvAction,HCoordWriteReplayAction,NodeFailureAction,HFollowerWriteReplayAction,HReadAction,HRcvAckAction"
