f9eab34aa02497f18d09d1edaa98da28bbd4dc30
William Schultz <will627@comcast.net>
2024-03-26 01:04:07 -0400

    Update AsyncRaft script

--- discovery_scripts/discovery_AsyncRaft_script.sh
+++ discovery_scripts/discovery_AsyncRaft_script.sh
@@ -2,26 +2,36 @@
 #SBATCH -J AsyncRaft_endive_Job               # Job name
 #SBATCH --partition short    # Partition name
 #SBATCH --time=12:00:00       # Time limit.
+#SBATCH --constraint="[broadwell|cascadelake]"       # Newer processors.
 #SBATCH -N 1                   # Number of nodes
 #SBATCH -n 1                   # Number of tasks
-#SBATCH --cpus-per-task 36                  # Number of cores
+#SBATCH --cpus-per-task 32                  # Number of cores
 #SBATCH -o endive_logs/AsyncRaft/AsyncRaft_output_%j.txt       # Standard output file
 #SBATCH -e endive_logs/AsyncRaft/AsyncRaft_error_%j.txt        # Standard error file
 
+specname="AsyncRaft"
+lscpu # log CPU info.
 module load OpenJDK/19.0.1
 mkdir -p benchmarking
 cd benchmarking
-mkdir -p AsyncRaft
-cd AsyncRaft
+mkdir -p $specname
+cd $specname
 # Clone if not already cloned.
 git clone -b ind-tree https://github.com/will62794/endive.git
 cd endive
 git pull --rebase
-srun python3 endive.py --spec benchmarks/AsyncRaft \
-    --seed 2444 --num_simulate_traces 30000 --tlc_workers 32 \
-    --debug --target_sample_time_limit_ms 10000 --target_sample_states 10000 \
-    --opt_quant_minimize --k_cti_induction_depth 1 --ninvs 40000 --max_num_ctis_per_round 1000 \
-     --save_dot --max_num_conjuncts_per_round 8 --niters 4 --override_num_cti_workers 6 \
+
+for seed in 1 2 3
+do
+echo "---"
+echo "--- Running '$specname' benchmark with seed $seed ---"
+echo "---"
+srun python3 endive.py --spec benchmarks/$specname \
+    --seed $seed --num_simulate_traces 200000 --tlc_workers 24 \
+    --debug --target_sample_time_limit_ms 10000 --target_sample_states 200000 \
+    --opt_quant_minimize --k_cti_induction_depth 1 --override_num_cti_workers 8 \
+    --ninvs 40000 --max_num_ctis_per_round 5000 \
+    --save_dot --max_num_conjuncts_per_round 8 --niters 4 \
     --auto_lemma_action_decomposition --enable_partitioned_state_caching --proof_tree_mode \
-    --nrounds 45 \
-    --action_filter "AdvanceCommitIndexAction,ClientRequestAction,AcceptAppendEntriesRequestLearnCommitAction,BecomeLeaderAction,HandleAppendEntriesResponseAction"
+    --nrounds 45
+done
