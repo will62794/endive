b66b4785fc93330c7005f1356863851638ebf8e6
William Schultz <will627@comcast.net>
2024-03-29 20:09:57 -0400

    Update more launch scripts

--- discovery_scripts/discovery_AsyncRaft_script.sh
+++ discovery_scripts/discovery_AsyncRaft_script.sh
@@ -5,9 +5,7 @@
 #SBATCH --constraint="[broadwell|cascadelake]"       # Newer processors.
 #SBATCH -N 1                   # Number of nodes
 #SBATCH -n 1                   # Number of tasks
-#SBATCH --cpus-per-task 32                  # Number of cores
-#SBATCH -o endive_logs/AsyncRaft/AsyncRaft_output_%j.txt       # Standard output file
-#SBATCH -e endive_logs/AsyncRaft/AsyncRaft_error_%j.txt        # Standard error file
+#SBATCH --cpus-per-task 28                  # Number of cores
 
 specname="AsyncRaft"
 lscpu # log CPU info.
@@ -21,17 +19,33 @@ git clone -b ind-tree https://github.com/will62794/endive.git
 cd endive
 git pull --rebase
 
-for seed in 1 2 3
+for seed in 2
 do
-echo "---"
-echo "--- Running '$specname' benchmark with seed $seed ---"
-echo "---"
-srun python3 endive.py --spec benchmarks/$specname \
-    --seed $seed --num_simulate_traces 200000 --tlc_workers 24 \
-    --debug --target_sample_time_limit_ms 10000 --target_sample_states 200000 \
-    --opt_quant_minimize --k_cti_induction_depth 1 --override_num_cti_workers 8 \
-    --ninvs 40000 --max_num_ctis_per_round 5000 \
-    --save_dot --max_num_conjuncts_per_round 8 --niters 4 \
-    --auto_lemma_action_decomposition --enable_partitioned_state_caching --proof_tree_mode \
-    --nrounds 45
+
+tlc_workers=24
+num_cti_workers=8
+nrounds=45
+ninvs=40000
+max_num_ctis_per_round=2000
+target_sample_states=200000
+num_simulate_traces=200000
+
+
+common_flags=" --seed $seed --tlc_workers $tlc_workers --override_num_cti_workers $num_cti_workers"
+common_flags+=" --debug --target_sample_time_limit_ms 10000 --num_simulate_traces $num_simulate_traces --target_sample_states $target_sample_states"
+common_flags+=" --opt_quant_minimize --k_cti_induction_depth 1"
+common_flags+=" --ninvs $ninvs --max_num_ctis_per_round $max_num_ctis_per_round"
+common_flags+=" --save_dot --max_num_conjuncts_per_round 16 --niters 4 --nrounds $nrounds"
+
+srun -o /home/schultz.w/endive_logs/${specname}/${specname}_output_%j_${seed}.txt \
+    python3 endive.py --spec benchmarks/$specname \
+    $common_flags --auto_lemma_action_decomposition --enable_partitioned_state_caching --proof_tree_mode 
+
+# srun -o /home/schultz.w/endive_logs/${specname}_baseline/${specname}_baseline_output_%j_${seed}.txt \
+#     python3 endive.py --spec benchmarks/$specname \
+#     $common_flags 
+
 done
+
+
+    # --action_filter "ClientRequestAction,AdvanceCommitIndexAction,AcceptAppendEntriesRequestLearnCommitAction,AcceptAppendEntriesRequestAppendAction,HandleAppendEntriesResponseAction"
