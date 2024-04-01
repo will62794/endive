b01371bc696c50a66eeeb38d63de77e5345048dc
William Schultz <will627@comcast.net>
2024-04-01 12:00:18 -0400

    Remove old fetching

--- discovery_fetch_data.sh
+++ discovery_fetch_data.sh
@@ -19,16 +19,6 @@ do
     scp -O -r "neudiscovery:$bmdir/$bm/endive/benchmarks/$bm*proofgraph*.json" $local_dir
 done
 
-scp -O -r "neudiscovery:$bmdir/Hermes/endive/benchmarks/Hermes_ind-proof-tree-sd*.pdf" $local_dir
-scp -O -r "neudiscovery:$bmdir/AsyncRaft/endive/benchmarks/AsyncRaft_ind-proof-tree-sd*.pdf" $local_dir
-scp -O -r "neudiscovery:$bmdir/Boulanger/endive/benchmarks/Boulanger_ind-proof-tree-sd*.pdf" $local_dir
-scp -O -r "neudiscovery:$bmdir/Bakery/endive/benchmarks/Bakery_ind-proof-tree-sd*.pdf" $local_dir
-scp -O -r "neudiscovery:$bmdir/consensus_epr/endive/benchmarks/consensus_epr_ind-proof-tree-sd*.pdf" $local_dir
-scp -O -r "neudiscovery:$bmdir/LamportMutex/endive/benchmarks/LamportMutex_ind-proof-tree-sd*.pdf" $local_dir
-scp -O -r "neudiscovery:$bmdir/TwoPhase/endive/benchmarks/TwoPhase_ind-proof-tree-sd*.pdf" $local_dir
-scp -O -r "neudiscovery:$bmdir/TwoPhase/endive/benchmarks/TwoPhase*proofgraph*.json" $local_dir
-scp -O -r "neudiscovery:$bmdir/ZeusReliableCommit/endive/benchmarks/ZeusReliableCommit_ind-proof-tree-sd*.pdf" $local_dir
-
 # Also fetch this other one.
 scp -O -r "neudiscovery:$bmdir/AsyncRaft_OnePrimaryPerTerm_2/endive/benchmarks/AsyncRaft_ind-proof-tree-sd2.pdf" $local_dir/AsyncRaft_OnePrimaryPerTerm_ind-proof-tree-sd2.pdf
 
