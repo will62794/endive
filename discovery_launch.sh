d51ca70260e4084af7c2c7b5570d716677fc5e63
William Schultz <will627@comcast.net>
2024-03-31 23:53:15 -0400

    Simplifying of some benchmark launching scripts

--- discovery_launch.sh
+++ discovery_launch.sh
@@ -9,11 +9,14 @@ scriptname="discovery_${specname}.script"
 
 # Copy the batch script to the Discovery cluster.
 echo "Copying script 'discovery_scripts/$scriptname' to Discovery cluster..."
+scp -O discovery_scripts/*.sh neudiscovery:/home/schultz.w
 scp -O discovery_scripts/$scriptname neudiscovery:/home/schultz.w
 
 # Launch the script.
 echo "Launching the script"
-ssh neudiscovery "sbatch $scriptname"
+jobname="${specname}_endive_Job"
+outfile="/scratch/schultz.w/endive_logs/${specname}_job_output_%j.txt"
+ssh neudiscovery "sbatch -J $jobname -o $outfile $scriptname"
 
 # Now list the running jobs on Discovery cluster.
 # ssh neudiscovery "squeue -u schultz.w"
