910b6aafed3749f91d0d243ea863a87cff4a3d26
William Schultz <will627@comcast.net>
2024-03-31 23:16:54 -0400

    Update some script dirs

--- discovery_fetch_data.sh
+++ discovery_fetch_data.sh
@@ -4,17 +4,17 @@
 #
 mkdir -p discovery_data
 
-scp -O -r neudiscovery:/home/schultz.w/endive_logs discovery_data
+scp -O -r neudiscovery:/scratch/schultz.w/endive_logs discovery_data
 
 # Also fetch Hermes,AsyncRaft proof stuff.
-scp -O -r "neudiscovery:/home/schultz.w/benchmarking/Hermes/endive/benchmarks/Hermes_ind-proof-tree-sd*.pdf" discovery_data
-scp -O -r "neudiscovery:/home/schultz.w/benchmarking/AsyncRaft/endive/benchmarks/AsyncRaft_ind-proof-tree-sd*.pdf" discovery_data
-scp -O -r "neudiscovery:/home/schultz.w/benchmarking/AsyncRaft_OnePrimaryPerTerm_2/endive/benchmarks/AsyncRaft_ind-proof-tree-sd2.pdf" discovery_data/AsyncRaft_OnePrimaryPerTerm_ind-proof-tree-sd2.pdf
-scp -O -r "neudiscovery:/home/schultz.w/benchmarking/Boulanger/endive/benchmarks/Boulanger_ind-proof-tree-sd*.pdf" discovery_data
-scp -O -r "neudiscovery:/home/schultz.w/benchmarking/Bakery/endive/benchmarks/Bakery_ind-proof-tree-sd*.pdf" discovery_data
-scp -O -r "neudiscovery:/home/schultz.w/benchmarking/consensus_epr/endive/benchmarks/consensus_epr_ind-proof-tree-sd*.pdf" discovery_data
-scp -O -r "neudiscovery:/home/schultz.w/benchmarking/ZeusReliableCommit/endive/benchmarks/ZeusReliableCommit_ind-proof-tree-sd*.pdf" discovery_data
+scp -O -r "neudiscovery:/scratch/schultz.w/benchmarking/Hermes/endive/benchmarks/Hermes_ind-proof-tree-sd*.pdf" discovery_data
+scp -O -r "neudiscovery:/scratch/schultz.w/benchmarking/AsyncRaft/endive/benchmarks/AsyncRaft_ind-proof-tree-sd*.pdf" discovery_data
+scp -O -r "neudiscovery:/scratch/schultz.w/benchmarking/AsyncRaft_OnePrimaryPerTerm_2/endive/benchmarks/AsyncRaft_ind-proof-tree-sd2.pdf" discovery_data/AsyncRaft_OnePrimaryPerTerm_ind-proof-tree-sd2.pdf
+scp -O -r "neudiscovery:/scratch/schultz.w/benchmarking/Boulanger/endive/benchmarks/Boulanger_ind-proof-tree-sd*.pdf" discovery_data
+scp -O -r "neudiscovery:/scratch/schultz.w/benchmarking/Bakery/endive/benchmarks/Bakery_ind-proof-tree-sd*.pdf" discovery_data
+scp -O -r "neudiscovery:/scratch/schultz.w/benchmarking/consensus_epr/endive/benchmarks/consensus_epr_ind-proof-tree-sd*.pdf" discovery_data
+scp -O -r "neudiscovery:/scratch/schultz.w/benchmarking/ZeusReliableCommit/endive/benchmarks/ZeusReliableCommit_ind-proof-tree-sd*.pdf" discovery_data
 
-scp -O -r "neudiscovery:/home/schultz.w/benchmarking/Boulanger/endive/benchmarks/Boulanger*proofgraph.json" discovery_data
+scp -O -r "neudiscovery:/scratch/schultz.w/benchmarking/Boulanger/endive/benchmarks/Boulanger*proofgraph.json" discovery_data
 
 ./discovery_job_status.sh
