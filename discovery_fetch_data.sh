117a1eb1e2dbe8a62bff877c5e01754a9acc03c0
William Schultz <will627@comcast.net>
2024-04-05 09:49:27 -0400

    save fetching

--- discovery_fetch_data.sh
+++ discovery_fetch_data.sh
@@ -13,27 +13,28 @@ mkdir -p $local_dir
 
 # Fetch proof graph stuff.
 bms="TwoPhase consensus_epr LamportMutex Hermes ZeusReliableCommit Boulanger Bakery AsyncRaft"
+# If list of arguments passed, use that instead.
+if [ $# -gt 0 ]; then
+    bms=$@
+fi
 echo "Fetching proof graphs."
 for bm in $bms
 do
     # Fetch rendered proof tree and proof graph object.
     scp -O -r "neudiscovery:$bmdir/${bm}_[0-9]/endive/benchmarks/${bm}_ind-proof-tree-sd*" $local_dir
     scp -O -r "neudiscovery:$bmdir/${bm}_[0-9]/endive/benchmarks/${bm}_IndDecompProof_*.tla" $local_dir
-    # TODO: Should be able to remove this second copy after proof grap renaming.
-    # scp -O -r "neudiscovery:$bmdir/$bm/endive/benchmarks/${bm}*proofgraph*.json" $local_dir
 done
 
 #
 # Also fetch these other ones.
 #
 
-for seed in 1 2 3 4 5 6
+for tag in "OnePrimaryPerTerm" "PrimaryHasEntriesItCreated"
 do
-    for tag in "OnePrimaryPerTerm" "PrimaryHasEntriesItCreated" "LogMatching"
+    for seed in 1 2 3 4 5 6 7 8
     do
     scp -O -r "neudiscovery:$bmdir/AsyncRaft_${tag}_$seed/endive/benchmarks/AsyncRaft_ind-proof-tree-sd$seed.pdf" $local_dir/AsyncRaft_${tag}_ind-proof-tree-sd$seed.pdf
-    scp -O -r "neudiscovery:$bmdir/AsyncRaft_${tag}_$seed/endive/benchmarks/AsyncRaft_IndDecompProof_$seed.tla" $local_dir/AsyncRaft_${tag}_IndDecompProof_$seed.tla
-    # scp -O -r "neudiscovery:$bmdir/AsyncRaft_LogMatching_$seed/endive/benchmarks/AsyncRaft_ind-proof-tree-sd$seed.pdf" $local_dir/AsyncRaft_LogMatching_ind-proof-tree-sd$seed.pdf
+    # scp -O -r "neudiscovery:$bmdir/AsyncRaft_${tag}_$seed/endive/benchmarks/AsyncRaft_IndDecompProof_$seed.tla" $local_dir/AsyncRaft_${tag}_IndDecompProof_$seed.tla
     done
 done
 
