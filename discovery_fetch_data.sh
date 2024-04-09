5badbe3a77ce822ff44e579867bbe12a76d8644a
William Schultz <will627@comcast.net>
2024-04-09 11:03:23 -0400

    save

--- discovery_fetch_data.sh
+++ discovery_fetch_data.sh
@@ -32,16 +32,19 @@ wait
 # Also fetch these other ones.
 #
 
-for tag in "OnePrimaryPerTerm" "PrimaryHasEntriesItCreated" "OnePrimaryPerTerm_broken"
-do
-    for seed in 1 2 3 4 5 6 7 8
+# Check for existence of "AsyncRaft" in bms.
+if [[ $bms == *"AsyncRaft"* ]]; then
+    for tag in "OnePrimaryPerTerm" "PrimaryHasEntriesItCreated" "OnePrimaryPerTerm_broken"
     do
-    scp -O -r "$xfer:$bmdir/AsyncRaft_${tag}_$seed/endive/benchmarks/AsyncRaft_ind-proof-tree-sd$seed.pdf" $local_dir/AsyncRaft_${tag}_ind-proof-tree-sd$seed.pdf &
-    scp -O -r "$xfer:$bmdir/AsyncRaft_${tag}_$seed/endive/benchmarks/AsyncRaft_ind-proof-tree-sd$seed.proofgraph.json" $local_dir/AsyncRaft_${tag}_ind-proof-tree-sd$seed.proofgraph.json &
-    # scp -O -r "neudiscovery:$bmdir/AsyncRaft_${tag}_$seed/endive/benchmarks/AsyncRaft_IndDecompProof_$seed.tla" $local_dir/AsyncRaft_${tag}_IndDecompProof_$seed.tla
+        for seed in 1 2 3 4 5 6 7 8
+        do
+        scp -O -r "$xfer:$bmdir/AsyncRaft_${tag}_$seed/endive/benchmarks/AsyncRaft_ind-proof-tree-sd$seed.pdf" $local_dir/AsyncRaft_${tag}_ind-proof-tree-sd$seed.pdf &
+        scp -O -r "$xfer:$bmdir/AsyncRaft_${tag}_$seed/endive/benchmarks/AsyncRaft_ind-proof-tree-sd$seed.proofgraph.json" $local_dir/AsyncRaft_${tag}_ind-proof-tree-sd$seed.proofgraph.json &
+        # scp -O -r "neudiscovery:$bmdir/AsyncRaft_${tag}_$seed/endive/benchmarks/AsyncRaft_IndDecompProof_$seed.tla" $local_dir/AsyncRaft_${tag}_IndDecompProof_$seed.tla
+        done
     done
-done
-wait
+    wait
+fi
 
 scp -O -r "$xfer:$bmdir/LamportMutex_broken_grammar/endive/benchmarks/LamportMutex_ind-proof-tree-sd3.pdf" $local_dir/LamportMutex_broken_grammar_ind-proof-tree-sd3.pdf
 scp -O -r "$xfer:$bmdir/consensus_epr_broken_grammar/endive/benchmarks/consensus_epr_ind-proof-tree-sd3.pdf" $local_dir/consensus_epr_broken_grammar_ind-proof-tree-sd3.pdf
