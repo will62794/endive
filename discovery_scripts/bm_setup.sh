d7097b0d64f562efbd479a51a7bd3c9b17ed919c
William Schultz <will627@comcast.net>
2024-04-20 21:56:04 -0400

    Add optional commands for installing tlapm on cluster machine

--- discovery_scripts/bm_setup.sh
+++ discovery_scripts/bm_setup.sh
@@ -5,6 +5,15 @@ lscpu # log CPU info.
 echo "Loading Java"
 module load OpenJDK/19.0.1
 
+#
+# To optionally install TLA+ proof manager, if we want to enable this for later use.
+#
+# cd /scratch/schultz.w
+# wget https://github.com/tlaplus/tlapm/releases/download/v1.4.5/tlaps-1.4.5-x86_64-linux-gnu-inst.bin
+# chmod +x tlaps-1.4.5-x86_64-linux-gnu-inst.bin
+# mkdir tlapm_install
+# ./tlaps-1.4.5-x86_64-linux-gnu-inst.bin -d /scratch/schultz.w/tlapm_install
+
 # Create work directory.
 bmdir="/scratch/schultz.w/benchmarking/$specname_arg"
 mkdir -p $bmdir
